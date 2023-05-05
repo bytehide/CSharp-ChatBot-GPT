6fbfb5f31f7538f0f4252c74489f0df961c971f6
Leo Golubyev <84777681+lgolubyev@users.noreply.github.com>
2023-05-05 19:17:04 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+namespace ChatGPTChatbot
+{
+    public class ChatGPTClient
+    {
+        private readonly string _apiKey;
+        private readonly RestClient _client;
+
+        // Constructor that takes the API key as a parameter
+        public ChatGPTClient(string apiKey)
+        {
+            _apiKey = apiKey;
+            // Initialize the RestClient with the ChatGPT API endpoint
+            _client = new RestClient("https://api.openai.com/v1/engines/text-davinci-003/completions");
+        }
+
+        // Method to send a message to the ChatGPT API and return the response
+        public string SendMessage(string message)
+        {
+            // Check for empty input
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                return "Sorry, I didn't receive any input. Please try again!";
+            }
+
+            try
+            {
+                // Create a new POST request
+                var request = new RestRequest("", Method.Post);
+                // Set the Content-Type header
+                request.AddHeader("Content-Type", "application/json");
+                // Set the Authorization header with the API key
+                request.AddHeader("Authorization", $"Bearer {_apiKey}");
+
+                // Create the request body with the message and other parameters
+                var requestBody = new
+                {
+                    prompt = message,
+                    max_tokens = 100,
+                    n = 1,
+                    stop = (string?)null,
+                    temperature = 0.7,
+                };
+
+                // Add the JSON body to the request
+                request.AddJsonBody(JsonConvert.SerializeObject(requestBody));
+
+                // Execute the request and receive the response
+                var response = _client.Execute(request);
+
+                // Deserialize the response JSON content
+                var jsonResponse = JsonConvert.DeserializeObject<dynamic>(response.Content ?? string.Empty);
+
+                // Extract and return the chatbot's response text
+                return jsonResponse?.choices[0]?.text?.ToString()?.Trim() ?? string.Empty;
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during the API request
+                Console.WriteLine($"Error: {ex.Message}");
+                return "Sorry, there was an error processing your request. Please try again later.";
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // Replace with your ChatGPT API key
+                string apiKey = "your_api_key_here";
+                // Create a ChatGPTClient instance with the API key
+                var chatGPTClient = new ChatGPTClient(apiKey);
+
+                // Display a welcome message
+                Console.WriteLine("Welcome to the ChatGPT chatbot! Type 'exit' to quit.");
+
+                // Enter a loop to take user input and display chatbot responses
+                while (true)
+                {
+                    // Prompt the user for input
+                    Console.ForegroundColor = ConsoleColor.Green; // Set text color to green
+                    Console.Write("You: ");
+                    Console.ResetColor(); // Reset text color to default
+                    string input = Console.ReadLine() ?? string.Empty;
+
+                    // Exit the loop if the user types "exit"
+                    if (input.ToLower() == "exit")
+                        break;
+
+                    // Send the user's input to the ChatGPT API and receive a response
+                    string response = chatGPTClient.SendMessage(input);
+
+                    // Display the chatbot's response
+                    Console.ForegroundColor = ConsoleColor.Blue; // Set text color to blue
+                    Console.Write("Chatbot: ");
+                    Console.ResetColor(); // Reset text color to default
+                    Console.WriteLine(response);
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# ChatGPT CSharp Bot
- This repository contains a simple C# chatbot powered by OpenAI’s ChatGPT. The chatbot utilizes the RestSharp and Newtonsoft.Json libraries to interact with the ChatGPT API and process user input.
+# ChatGPT C# Chatbot
+
+This repository contains a simple C# chatbot powered by OpenAI's ChatGPT. The chatbot utilizes the RestSharp and Newtonsoft.Json libraries to interact with the ChatGPT API and process user input.
+
+## How to Use
+
+1. Clone this repository or copy the provided code.
+2. Ensure you have the required NuGet packages installed:
+    - RestSharp
+    - Newtonsoft.Json
+3. Replace the placeholder API key in the `Main` method with your ChatGPT API key.
+4. Run the application to start interacting with the ChatGPT chatbot.
+
+## Code Explanation
+
+For a detailed step-by-step guide on how this code works and how to create a ChatGPT chatbot in C#, please refer to the following article: [Creating a Chatbot with ChatGPT and C#](https://www.bytehide.com/blog/chatbot-chatgpt-csharp)
+
+## Features
+
+- User-friendly console interface with colored prompts.
+- Easy-to-understand code structure for quick customization.
+- Simple API interaction with the ChatGPT API using RestSharp.
